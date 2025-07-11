05fcbffe47c879fecd7d7ff8dc036f3a778f0a1c
suraj8817 <dhakadsurajgwl8817@gmail.com>
2025-07-11 17:42:44 +0530

    data science

---
+++ python/basic.py.py
@@ -0,0 +1,204 @@
+
+###slicing in python
+
+
+#name = "suraj"
+#print(name[0:4])
+
+# how work
+
+
+### operaiton
+#name = "suraj"
+#upper_case =name.upper()### upper function ka used upper case ke liye karte h
+
+#print(upper_case)
+#lower_last_name_=last.lower()
+# print(lower_last_name)
+
+#print(name.count("r"))
+
+
+#>>>>>>>>>>>>>>>>>>>.list<<<<<<<<<<<<<<<<<<
+#list=
+# = [1,2,3,4,5,6,7, "suraj",2,3,4,]###>>>>>>>>>>>>>>>>list
+#array or list>>>>>>
+# duplicated values
+# ## herto
+# order 
+# array >>>>>>>>>> lst - {1,2,3,4,5,6]
+
+#array
+#print("my first list:-",lst)
+#print ("len of list:-",len(lst))
+#print("type of list:-",type(lst))
+#list=[1,2,3,4,]
+#print(list)
+#print(lst[0])
+#print(lst[5])
+
+#print(lst[0:5])
+#lst = [1,2,3,4,5,6,7, "suraj", 2.4]
+#lst.pop()
+#print(lst)
+# lst.append(100)
+#print(lst)
+#lst.insert(0,1000)
+#copy_lst =lst.copy()
+#print(copy_lst)
+#lst.reverse()
+#lst.remove(2.9)
+#print(lst)
+ 
+
+#lst.sort()
+
+
+#lst = [1,2,2,3,4,5,6,7, 2.3]
+ 
+#print(lst.count(2))
+#print(lst)
+
+
+
+
+
+
+
+
+# >>>>>>>>>>>.TUPLE>>>>>>>>>>>>>>>>
+
+#tuple =
+#tuple = (1,2,3,4,345,4, "suraj", 2.3)
+#print("my first tuple:-",tpl)
+#print("len of tuple:-",len(tpl))
+#print("type of tuple:-",type(tpl))
+
+#print(tpl[0])
+#print(tpl[2])
+#print(tpl[0:6])
+
+# a = 1,2,3,4,4,55,6
+#print(a)
+#print(type(a))
+# print(len(a))
+
+
+#a , b, c =(1,2,3)
+#print(a)
+#print(b)
+#print(c)
+ 
+
+#a,b,c =1,2,3
+#print(a)
+#print(b)
+#print(c)
+
+#tpl=(1,2,3,23,344.)
+#print(sum)
+#print("max of the tuple is"max(tpl))
+#print(("min"))
+
+
+
+
+#my_dict ={
+  #  "name":"suraj"
+  #  "class": "2nd year"
+   # "Roll number": "23ecacs050"
+#"Address": "jaipur","name","suraj"
+#}
+#my_dict['branch']="cse"
+#print(my_dict)
+
+
+
+
+
+
+
+
+#print("my first dict :-",my_dict)
+#print("len of dict :-",len(my_dict))
+#print("type of dict :-",type(my_dict))
+#print(my_['name'])
+#print(my_dict['class'])
+
+#print(my_['roll number'])
+#print(my_dict['Address'])
+
+#my_dict['Address']="new jaipur"
+#print(my_dict)
+
+
+
+# task1 >>>>>>>>>> update function
+# get function  >>>>>> 5 or []
+
+
+# print(my_dict.key())
+# print(my_dict.values())
+# print(my_dict.items())
+
+#copy_dict =my_dict.copy()
+#print(copy_dict)
+
+#my_dict.clear
+# print(my_dict)
+
+#a =input("enter a number")
+#b =input("enter second number")
+#print(a)
+#print(a*b)
+#print(type(a))
+#print(type(a))
+
+
+#a =input("enter a number")
+#b =input("enter second number")
+#print(a*b)
+
+# lst =[1,2,3,4,32,]
+# print("this is my list:-",lst)
+# print("type of list:-",type(1st))
+# tpl =tuple(lst)
+# print("type of tuple :-",type(tpl))
+# print("type of tuple:-",type(tpl))
+
+
+#>>>>>>>>>>>>>>>> set. >>>>>>>>>>>>
+# my_set =
+# tuple =
+# list=
+# set =
+# dict =
+
+# my_set ={1,2,3,4,5,354, "jai"}
+# print("this is my first set:-",my_set)
+# print("len of my set:-",len(my_set))
+# print("type of set:-",type(my_set))
+
+# task<<<<<<<<<<<<<<
+# my_set.union()
+# my_set.intersection()
+# my_set.difference()
+
+# lst =list(my_set)
+
+# lst.append(1000)
+# print(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
